b654338bee5233442d101e6b219729bf0d53ee7a
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-22 03:49:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+   def name=(first_name)
+     @name= first_name
+    end
+    def name
+      @name
+    end
+    def job=(work)
+      @job= work
+     end
+    def job
+     @job
+    end
+
+
+end
