6c469c81347dfe7e344045c37def2f9431f66ae5
Rafael Vargas <vargas718@gmail.com>
2019-02-07 16:51:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog 
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(type_of_dog)
+    @breed = type_of_dog
+  end
+  
+  def breed
+    @breed
+  end
+    
+  
+  
+  
+  
+  
+  
+  
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  
+  def name=(new_name)  #writer method 
+  @name = new_name 
+end
+
+def name # reader method
+
+  @name
+
+end
+
+def job=(job_title) # writer method
+ 
+  @job = job_title 
+  
+end
+
+def job # reader method 
+
+  @job 
+
+end
+
+  
+end
